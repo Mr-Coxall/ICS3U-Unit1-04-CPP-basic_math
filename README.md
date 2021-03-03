8f9e80ae84768b60721bb47db21fc9d3771ca71f
Mr. Coxall <patrick.coxall@ocsb.ca>
2021-03-03 03:15:58 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ICS3U-Unit1-04-CPP-basic_math
+# ICS3U Unit #1-04 CPP → basic_math
+[![GitHub's Super Linter](https://github.com/Mr-Coxall/ICS3U-Unit1-04-CPP-area_and_perimeter/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Mr-Coxall/ICS3U-Unit1-04-CPP-area_and_perimeter/actions)
---
+++ area_and_perimeter.cpp
@@ -0,0 +1,18 @@
+// Created by: Mr. Coxall
+// Created on: Sep 2020
+// This program does basic math
+
+#include <iostream>
+#include <math.h>       /* pow */
+
+int main() {
+	std::cout << "9+2=" << (9 + 2) << std::endl;
+    std::cout << "7-2=" << (7 - 2) << std::endl;
+    std::cout << "4/2=" << (4 / 2) << std::endl;
+    std::cout << "4+4/2=" << (4 + 4 / 2) << std::endl;
+    std::cout << "3+4*2=" << (3 + 4 * 2) << std::endl;
+    std::cout << "5+2^3=" << (5 + 2 ^ 3) << std::endl;
+    std::cout << "5+pow(2,3)=" << (5 + pow(2, 3)) << std::endl;
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
